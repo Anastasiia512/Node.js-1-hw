f133081fa6b37eb91f977aa7dd6be3e3a2516567
Anastasiia <nastyona221985@gmail.com>
2020-09-10 14:15:07 +0300

    last checking done

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const passport = require("passport");
+const passport = require('passport');
 require("dotenv").config();
 require("./passport").initializeStrategies();
 
